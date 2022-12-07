a00099e2c98e6d2117fbaa799f4bd09a96f24fdc
charliram <83847746+charliram@users.noreply.github.com>
2022-12-07 14:06:14 -0600

    final charly

--- BACK/src/controladores/carrito.js
+++ BACK/src/controladores/carrito.js
@@ -21,6 +21,7 @@ carritoControlador.totalXcarrito = async (req, res) =>{
 }
 
 carritoControlador.addCarrito = async (req, res) => {
+    console.log(req.body);
     //let sql = 'insert into carrito (ID_User,ID_Producto,Cantidad,Sabor,Comentario,Total,Enviado) values (' + req.body.usuario + ',' + req.body.producto + ',' + req.body.cantidad + ',"' + req.body.sabor + '","' + req.body.comentario + '",' + req.body.total + ',false)'
     let sql='call P_Carrito('+ req.body.usuario + ',' + req.body.producto + ',' + req.body.cantidad + ',"' + req.body.sabor + '","' + req.body.comentario + '",' + req.body.total+',false)'
    
--- FRONT/src/app/General/navbar/navbar.component.ts
+++ FRONT/src/app/General/navbar/navbar.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { datosCatalogo } from 'src/app/Interfaces/datosCatalogo.interface';
 import { CafeteriasService } from 'src/app/Servicios/cafeterias.service';
+import { CarritoService } from 'src/app/Servicios/carrito.service';
 import { CatalogosService } from 'src/app/Servicios/catalogos.service';
+import { ProductosService } from 'src/app/Servicios/productos.service';
 import { UsuariosService } from 'src/app/Servicios/usuarios.service';
 
 
@@ -14,11 +16,15 @@ import { UsuariosService } from 'src/app/Servicios/usuarios.service';
 export class NavbarComponent implements OnInit {
  
   
-  constructor(private ServicioUsuarios:UsuariosService ) { }
+  constructor(private ServicioUsuarios:UsuariosService, private ServicioCarrito:CarritoService, private ServicioProducto:ProductosService) { }
    id:any;
    tipo:number=0;
+   carrito: any=[];
+   nombres: string[]=[];
+   contador:number=1;
 
   ngOnInit(): void {
+
     let aux: any;
     this.id=sessionStorage.getItem('id');
     if(sessionStorage.getItem('tipo')!=null){
@@ -26,9 +32,40 @@ export class NavbarComponent implements OnInit {
       this.tipo=parseInt(aux);
       console.log(typeof(this.tipo));
     }
-    console.log(this.id);
-    console.log(this.tipo);
+   
+
+    this.ServicioCarrito.carritoUsuario(this.id).subscribe(async (res:any)=>{
+      if(res.ok==true){
+       
+        for(let i of res.info){
+          this.carrito.push(i);
+
+          this.ServicioProducto.productoXid(i.ID_Producto).subscribe(async (res:any)=>{
+            if(res.ok==true){
+              for(let i of res.info){
+                this.nombres.push(i.Producto);
+                console.log(i.Producto);
+            }
+            }
+          })
+      }
+      console.log(typeof(this.nombres));
+      console.log(this.carrito);
+      
+      }
+    })
   }
+
+  mas(): void{
+    this.contador++;
+  }
+  menos(): void{
+    if(this.contador>1){
+      this.contador--;
+    }
+    
+  }
+
   
 
 }
